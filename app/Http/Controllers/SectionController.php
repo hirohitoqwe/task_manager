ae8e6cfbdad9b2d993aecfcfa432c37b540ee9e9
hirohitoqwe <balduin@internet.ru>
2023-01-31 01:10:39 +0300

    section controller

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Section;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SectionController extends Controller
 {
-    //
+    public function createSection(Request $request): JsonResponse
+    {
+        $section = new Section();
+        $section->section_name = $request->section_name;
+        $section->save();
+        return response()->json($section, 201);
+    }
+
+    public function deleteSection(int $id): JsonResponse
+    {
+        Section::destroy($id);
+        return response()->json(['message' => 'Section was deleted'], 202);
+    }
+
 }
