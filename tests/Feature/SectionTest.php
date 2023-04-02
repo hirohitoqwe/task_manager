4ef2195e1d4f36817c7134b8fd394ba2ee2d4ea0
hirohitoqwe <balduin@internet.ru>
2023-04-02 19:54:18 +0300

    added test

--- tests/Feature/SectionTest.php
+++ tests/Feature/SectionTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 
+use App\Models\Section;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -36,7 +37,7 @@ class sectionTest extends TestCase
         $response->assertStatus(201);
     }
 
-    public function test_section_by_user()
+    public function test_section_by_user()// and section by user not isset, because we dont created his
     {
         $user = User::factory()->create();
         Sanctum::actingAs(
@@ -45,6 +46,21 @@ class sectionTest extends TestCase
         );
         $response = $this->get("api/section/{$user->id}");
         $response->assertStatus(404);
+    }
+
+    public function test_section_by_user()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*']
+        );
+        $section = new Section();
+        $section->section_name = 'test';
+        $section->user_id = $user->id;
+        $section->save();
+        $response = $this->get("/api/section/{$user->id}");
+        $response->assertStatus(200);
     }
 
 }
