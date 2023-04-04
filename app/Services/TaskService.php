4e67cac7f9ef56c997666c510129a06db9df29ba
hirohitoqwe <balduin@internet.ru>
2023-04-04 18:41:27 +0300

    task service refactor, first test

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -12,11 +12,11 @@ class TaskService
         try {
             $task = new Task();
             $task->task_name = $data['task_name'];
-            $task->user_id = $data['user_id'] ?? null;
+            $task->user_id = $data['user_id'];
             $task->section_id = $data['section_id'] ?? null;
             $task->task_status = true;
             $task->save();
-            return response()->json($task, 200);
+            return response()->json($task, 201);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Create Task Exception'], 400);
         }
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
@@ -19,4 +21,16 @@ class TaskTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_create_task()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*']
+        );
+        $response = $this->post('/api/task', ['task_name' => 'test_task', 'user_id' => $user->id]);
+        $response->assertStatus(201);
+    }
+
 }
